9a9fca2ba0fe89e598ccf425f7cf75b46925a849
laura joan feyrer <33075830+ljfeyrer@users.noreply.github.com>
2023-10-05 13:02:35 -0700

    Update LatLong_convert.R

--- scripts/LatLong_convert.R
+++ scripts/LatLong_convert.R
@@ -6,14 +6,22 @@
 library(readr)
 library(here)
 
-#read in csv of data with Degrees Decimal Minute Lat/Long in separate variables and out put dataset of Decimal Degrees
+#read in csv of data with Degrees Decimal Minute Lat/Long in the first 4 separate variables 
 
-DM_coords = read_csv(here("input", "LatLong_data.csv"), col_types = cols(.default ="c")) %>%
-  mutate(LATITUDE.D = as.numeric(gsub("[^0-9.-]", "", `Lat. Degrees`)), LATITUDE.M =as.numeric(gsub("[^0-9.-]", "", Lat.Min.m)),
-         LONGITUDE.D = as.numeric(gsub("[^0-9.-]", "", `Long. Degrees`)), LONGITUDE.M = as.numeric(gsub("[^0-9.-]", "", Long.Min.m) ))%>% 
-  mutate(LATITUDE = (LATITUDE.D+(LATITUDE.M/60)), LONGITUDE = (LONGITUDE.D+(LONGITUDE.M/60))*-1)%>%dplyr::select(LATITUDE, LONGITUDE)
+DM_coords = read_csv(here("input", "LatLong_data.csv"), col_types = cols(.default ="c")) 
+# rename so variables are generic
+colnames(DM_coords)[1:4] <- paste0("new_col", 1:4)
+
+#convert DdM to DD
+
+DM_coords = DM_coords%>%  mutate(across(.cols = 1:4, .fns = ~ as.numeric(gsub("[^0-9.-]", "", .)))) %>% 
+  mutate(LATITUDE = new_col1 + (new_col2 / 60),
+    LONGITUDE = new_col3 + (new_col4 / 60) * -1
+  ) %>%
+  dplyr::select(LATITUDE, LONGITUDE)
 
 #save output in output directory as csv
+print(DM_coords)
 
 write.csv(DM_coords, here("output", "LatLong_data.csv"))
 
