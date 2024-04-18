cdab9d207687f6662d78a98153f83f7f4dfee103
megmurph303 <megan.murphy@dfo-mpo.gc.ca>
2024-04-18 11:22:24 -0300

    Joy's updates
    Incorporated Joy's suggestions/changes to script

--- scripts/WSDBtoCSDB_MM.R
+++ scripts/WSDBtoCSDB_MM.R
@@ -11,17 +11,12 @@ input_file_path <- paste0(r"(R:\Science\CetaceanOPPNoise\CetaceanOPPNoise_12\WSD
 WSDB_data <- read_csv(input_file_path, locale = readr::locale(encoding = "Cp1252"))
 
 # Read data tables
-Species_code_table <- r"(R:\Science\CetaceanOPPNoise\CetaceanOPPNoise_12\WSDB\WSDB_to_CSDB\Code_tables\Species_code_table.csv)"
-Species_code_table <- read_csv(Species_code_table)
+code_table_path <- r"(R:\Science\CetaceanOPPNoise\CetaceanOPPNoise_12\WSDB\WSDB_to_CSDB\Code_tables\)"
 
-Activity_type_code_table <- r"(R:\Science\CetaceanOPPNoise\CetaceanOPPNoise_12\WSDB\WSDB_to_CSDB\Code_tables\Activity_type_code_table.csv)"
-Activity_type_code_table <- read_csv(Activity_type_code_table)
-
-Effort_code_table <- r"(R:\Science\CetaceanOPPNoise\CetaceanOPPNoise_12\WSDB\WSDB_to_CSDB\Code_tables\Effort_code_table.csv)"
-Effort_code_table <- read_csv(Effort_code_table)
-
-Data_source_code_table  <- r"(R:\Science\CetaceanOPPNoise\CetaceanOPPNoise_12\WSDB\WSDB_to_CSDB\Code_tables\Data_source_code_table.csv)"
-Data_source_code_table <- read_csv(Data_source_code_table)
+Species_code_table <- read_csv(paste0(code_table_path, "Species_code_table.csv"))
+Activity_type_code_table <- read_csv(paste0(code_table_path, "Activity_type_code_table.csv"))
+Effort_code_table <- read_csv(paste0(code_table_path, "Effort_code_table.csv"))
+Data_source_code_table <- read_csv(paste0(code_table_path, "Data_source_code_table.csv"))
 
 # Start CSDB dataframe----
 # 1) Format Year Month and Day to pull from UTC_Year, UTC_Month, UTC_Day when available, if not use Reported_Year, Reported_Month, Reported_Day
@@ -29,7 +24,7 @@ Data_source_code_table <- read_csv(Data_source_code_table)
 CSDB_data <- WSDB_data %>%
   mutate(Year= ifelse(is.na(UTC_Year),Reported_Year, UTC_Year),
          Month= ifelse(is.na(UTC_Month),Reported_Month, UTC_Month),
-         Day= ifelse(is.na(UTC_Day),Reported_Day, Reported_Day))
+         Day= ifelse(is.na(UTC_Day),Reported_Day, UTC_Day))
 
 # 2) Modify WS_EVENT_ID to Regional_Primary_Key so that the first 2 digits are “11” (representing MAR Region) and the total number of digits is 11 (including the WS_EVENT_ID)
 # example: if WS_EVENT_ID is 1234, then the Regional_Primary_Key should be 11000001234  
@@ -38,36 +33,36 @@ CSDB_data <- CSDB_data %>%
 
 # 3) Change the format of UTC_Time and Reported_Time to hh:mm:ss
 CSDB_data <- CSDB_data %>%
-  mutate(UTC_Time = sprintf("%04d", WS_TIME_UTC),
-         UTC_Time = parse_date_time(UTC_Time, orders= "HM", quiet = T),
+  mutate(UTC_Time = ifelse(!is.na(WS_TIME_UTC), sprintf("%04d", WS_TIME_UTC), NA),
+         UTC_Time = parse_date_time(UTC_Time, orders= "HM"),
          UTC_Time = format(UTC_Time, format = "%H:%M:%S"))
 
 CSDB_data <- CSDB_data %>%
-  mutate(Reported_Time = sprintf("%04d", WS_TIME),
-         Reported_Time = parse_date_time(Reported_Time,orders= "HM", quiet = T),
+  mutate(Reported_Time = ifelse(!is.na(WS_TIME), sprintf("%04d", WS_TIME), NA),
+         Reported_Time = parse_date_time(Reported_Time, orders= "HM"),
          Reported_Time = format(Reported_Time, format = "%H:%M:%S"))
 
 # 4) Map LCQECODE_CD to Location_Uncertainty. Note: Location_Uncertainty codes that are identical to LCQECODE_CDs remain unchanged 
 # example: LCQECODE_CD = 1, Location_Uncertainty = 1
 CSDB_data <- CSDB_data %>%
   mutate(Location_Uncertainty = case_when(LCQECODE_CD == 4 ~ 3, 
-                                               LCQECODE_CD == 5 ~ 3, 
-                                               LCQECODE_CD == 6 ~ 3, 
-                                               LCQECODE_CD == 7 ~ 3,
-                                               is.na(LCQECODE_CD) ~ 0,
-                                               TRUE ~ as.numeric(LCQECODE_CD)))
+                                          LCQECODE_CD == 5 ~ 3, 
+                                          LCQECODE_CD == 6 ~ 3, 
+                                          LCQECODE_CD == 7 ~ 3,
+                                          is.na(LCQECODE_CD) ~ 0,
+                                          TRUE ~ as.numeric(LCQECODE_CD)))
 
 # 5) Map LCQECODE_CD to Location_Uncertainty_Reason
 CSDB_data <- CSDB_data %>%
   mutate(Location_Uncertainty_Reason = case_when(LCQECODE_CD == 3 ~ 1, 
-                                                      LCQECODE_CD == 4 ~ 2, 
-                                                      LCQECODE_CD == 5 ~ 3, 
-                                                      LCQECODE_CD == 6 ~ 4, 
-                                                      LCQECODE_CD == 7 ~ 5,
-                                                      is.na(LCQECODE_CD) ~ 0,
-                                                      LCQECODE_CD == 1 ~ NA_real_,
-                                                      LCQECODE_CD == 2 ~ NA_real_,
-                                                      TRUE ~ as.numeric(LCQECODE_CD)))
+                                                 LCQECODE_CD == 4 ~ 2, 
+                                                 LCQECODE_CD == 5 ~ 3, 
+                                                 LCQECODE_CD == 6 ~ 4, 
+                                                 LCQECODE_CD == 7 ~ 5,
+                                                 is.na(LCQECODE_CD) ~ 0,
+                                                 LCQECODE_CD == 1 ~ NA_real_,
+                                                 LCQECODE_CD == 2 ~ NA_real_,
+                                                 TRUE ~ as.numeric(LCQECODE_CD)))
 
 # 6) Map COMMONNAME to ITIS_Code and Species_code using Species_code_table
 CSDB_data <- CSDB_data %>%
@@ -89,8 +84,8 @@ CSDB_data <- CSDB_data %>%
 # example: IDREL_CD = 1, SpeciesID_Uncertainty = 1.
 CSDB_data <- CSDB_data %>%
   mutate(SpeciesID_Uncertainty = case_when(IDREL_CD == 9 ~ 0,
-                                                is.na(IDREL_CD) ~ 0,
-                                                TRUE ~ as.numeric(IDREL_CD)))
+                                           is.na(IDREL_CD) ~ 0,
+                                           TRUE ~ as.numeric(IDREL_CD)))
 
 # 11) Map COUNT_UNCERTAINTY_CD to Count_Uncertainty (put 0’s in for null)
 CSDB_data <- CSDB_data %>%
@@ -99,20 +94,16 @@ CSDB_data <- CSDB_data %>%
 # 12) Create column called Behaviour_Comments that concatenates the five BEHAVIOUR_DESC columns into the one column with hyphens in between
 # 12a) Change 'NOT RECORDED' to NA from BEHAVIOUR_DESC columns
 CSDB_data = CSDB_data %>%
-  mutate(BEHAVIOUR_DESC = case_when(BEHAVIOUR_DESC == "NOT RECORDED" ~ NA,
-                                    .default = BEHAVIOUR_DESC),
-         BEHAVIOUR_DESC_1 = case_when(BEHAVIOUR_DESC_1 == "NOT RECORDED" ~ NA,
-                                      .default = BEHAVIOUR_DESC_1),
-         BEHAVIOUR_DESC_2 = case_when(BEHAVIOUR_DESC_2 == "NOT RECORDED" ~ NA,
-                                      .default = BEHAVIOUR_DESC_2),
-         BEHAVIOUR_DESC_3 = case_when(BEHAVIOUR_DESC_3 == "NOT RECORDED" ~ NA,
-                                      .default = BEHAVIOUR_DESC_3),
-         BEHAVIOUR_DESC_4 = case_when(BEHAVIOUR_DESC_4 == "NOT RECORDED" ~ NA,
-                                      .default = BEHAVIOUR_DESC_4))
+  
+  ##JS: option to do this in fewer lines of code
+  
+  mutate(across(starts_with("BEHAVIOUR_DESC"),~ case_when(. == "NOT RECORDED" ~ NA_character_,
+                                                          TRUE ~ .), .names = "new_{.col}"))
 
 # 12b) Concatenate the five BEHAVIOUR_DESC columns separated by hyphens into a new column called Behaviour_Comments, removing NA's
 CSDB_data <- CSDB_data %>%
-  unite('Behaviour_Comments', c('BEHAVIOUR_DESC','BEHAVIOUR_DESC_1','BEHAVIOUR_DESC_2','BEHAVIOUR_DESC_3','BEHAVIOUR_DESC_4'), sep=" - ", na.rm = TRUE)
+  
+  unite('Behaviour_Comments', c('new_BEHAVIOUR_DESC','new_BEHAVIOUR_DESC_1','new_BEHAVIOUR_DESC_2','new_BEHAVIOUR_DESC_3','new_BEHAVIOUR_DESC_4'), sep=" - ", na.rm = TRUE)
 
 # 13) Map Animal_Status_Code using GEARIMPACT_CD and Behaviour_Comments
 CSDB_data <- CSDB_data %>%
@@ -159,15 +150,15 @@ CSDB_data <- CSDB_data %>%
   mutate(Latitude = sprintf(LATITUDE, fmt = '%.4f'))
 
 CSDB_data <- CSDB_data %>%
-  mutate(Longitude = sprintf(LONGITUDE, fmt = '%.4f'))
-
-# 20) Remove commas from Comments and Behaviour_Comments
-CSDB_data <- CSDB_data %>%
+  mutate(Longitude = sprintf(LONGITUDE, fmt = '%.4f')) %>% 
+  
+  # 20) Remove commas from Comments and Behaviour_Comments
+  
   mutate(Comments = str_replace_all(COMMENTS,",",""),
          Behaviour_Comments = str_replace_all(Behaviour_Comments, ",",""))
 
 # 21) Select only the columns needed for CSDB data output in the desired order
-CSDB_data <- CSDB_data %>%
+CSDB_data_final <- CSDB_data %>%
   dplyr::select(Regional_Primary_Key, Year, Month, Day, UTC_Time, Reported_Time, Latitude, Longitude, Location_Uncertainty, Location_Uncertainty_Reason, 
                 Species_Code, ITIS_Code, SpeciesID_Uncertainty, Species_Comments, Reported_Count, Min_Count, Max_Count, Count_Uncertainty, Animal_Status_Code,
                 Behaviour_Comments, Distance, Reported_SeaState, Platform_Type_Code, Activity_Type_Code, Effort, Data_Source_Code, Suspected_Data_Issue, Suspected_Data_Issue_Reason, Comments)
@@ -176,4 +167,3 @@ CSDB_data <- CSDB_data %>%
 today <- Sys.Date()
 output_file = paste0("CSDB_", filename, "_", today, ".xlsx")
 write_xlsx(CSDB_data, paste0(r"(R:\Science\CetaceanOPPNoise\CetaceanOPPNoise_12\WSDB\WSDB_to_CSDB\Output\)", output_file))
-
